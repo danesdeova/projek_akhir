ad9bc764f4c541db8535a745814c1a100980178e
danesdeova <danesdeova01@student.ub.ac.id>
2022-12-12 15:39:36 +0700

    first

---
+++ Final Project/ArrayMultidimensi/src/arraymultidimensi/ArrayMultidimensi.java
@@ -0,0 +1,31 @@
+package arraymultidimensi;
+public class ArrayMultidimensi {
+
+    public static void main(String[] args) {
+         System.out.println("       Negara-Negara ASEAN       ");
+         System.out.println("------------------------------------");
+     
+         String [][] asean = {
+         
+         {"Indonesia", "Filipina", "Singapura","Thailand", "Vietnam", "Malaysia", "Kamboja", "Brunei", "Laos", "Myanmar", "Timor Leste"},
+         {"Bangkok", "Hanoi", "Jakarta", "Phnom Penh", "Singapura", "Manila", "Kuala Lumpur", "Dilli", "Rangoon", "Vientiane", "Bandar Seri Begawan"},
+         {" Ibu Kota => "}
+                 
+         };
+         System.out.println(asean [0][0] + asean [2][0] + asean [1][2]);
+         System.out.println(asean [0][1] + asean [2][0] + asean [1][5]);
+         System.out.println(asean [0][2] + asean [2][0] + asean [1][4]);
+         System.out.println(asean [0][3] + asean [2][0] + asean [1][0]);
+         System.out.println(asean [0][4] + asean [2][0] + asean [1][1]);
+         System.out.println(asean [0][5] + asean [2][0] + asean [1][6]);
+         System.out.println(asean [0][6] + asean [2][0] + asean [1][3]);
+         System.out.println(asean [0][7] + asean [2][0] + asean [1][10]);
+         System.out.println(asean [0][8] + asean [2][0] + asean [1][9]);
+         System.out.println(asean [0][9] + asean [2][0] + asean [1][8]);
+         System.out.println(asean [0][10] + asean [2][0] + asean [1][7] );
+         
+        }
+    }
+
+//Danes_ArrayMultidimensi_PTI-A_Praktikum//
+
